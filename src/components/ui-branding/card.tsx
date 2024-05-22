a1a6f7b5b35ffb295f9d891ffadf3834f99bc401
Eduardo Botelho <eduardo11224b@gmail.com>
2024-05-22 21:09:20 +0100

    font size

--- src/components/ui-branding/card.tsx
+++ src/components/ui-branding/card.tsx
@@ -11,11 +11,11 @@ interface Props {
 export default function Card(props: Props) {
   const { icon, title, paragraph } = props.data;
   return (
-    <article className="w-full xl:w-[380px] h-[290px] bg-white p-9 flex flex-col gap-4">
+    <article className="w-full xl:w-[380px] bg-white p-9 flex flex-col gap-4">
       <span className="text-4xl text-[#97959a]">
         <IconComponent icon={icon} />
       </span>
-      <h1 className="text-2xl font-semibold">{title}</h1>
+      <h1 className="text-2xl font-semibold text-[22px]">{title}</h1>
       <p className="font-light">{paragraph}</p>
     </article>
   );
